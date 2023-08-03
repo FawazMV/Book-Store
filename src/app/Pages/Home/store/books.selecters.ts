70bc9f03b4d96692a31f2d3d80e8fa3c8a25a4df
Muhammed Fawaz MV <mvfawazmfz@gmail.com>
2023-08-03 14:42:03 +0530

    updates

--- src/app/Pages/Home/store/books.selecters.ts
+++ src/app/Pages/Home/store/books.selecters.ts
@@ -6,23 +6,3 @@ export const bookSelector = createSelector(
   (state: BooksState) => state.books,
   (books: ReadonlyArray<BooksModle>) => books
 )
-
-// const getSBooksState = createFeatureSelector<AppState>('books')
-
-// export const singleBook = createSelector(
-//   (state: BooksState) => state.books,
-//   (books: ReadonlyArray<BooksModle>, props: { id: string }) =>
-//     books.find(book => book.id === props.id)
-// )
-// export const singleBookDetails = (id: string) =>
-//   createSelector(
-//     (state: BooksState) => state.books,
-//     (books: ReadonlyArray<BooksModle>) => books.find(book => book.id === id)
-//   )
-
-// export const singleBookDetails = createSelector(
-//   getSBooksState,
-//   (state: AppState, props: { id: string }) => {
-//     const valu = state.books.books.find(book => book.isbn13 === props.id)
-//   }
-// )
